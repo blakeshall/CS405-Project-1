ff502d61b2e185cf23c3a7dded739a7294cf9aba
blakeshall <blake@blakeshall.com>
2011-10-03 09:38:22 -0400

    Added create and index methods to payments controller

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,2 +1,19 @@
 class PaymentsController < ApplicationController
+  def create
+    @service =Service.find(params[:service_id])
+    @payment = @service.payments.create(params[:payment])
+    redirect_to service_path(@service)
+
+  end
+
+  def index
+    @service = Service.find(params[:service_id])
+    @payments = @service.payments
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @service }
+    end
+  end
+
+  
 end
