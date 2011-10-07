d8dbcf3b943b7ebb923c8bda950833520f6a75db
blakeshall <blake@blakeshall.com>
2011-10-07 11:12:34 -0400

    Moved yearly payment methods to the payment class

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -1,3 +1,29 @@
 class Payment < ActiveRecord::Base
   belongs_to :service
+
+  def self.month_payment(month)
+    # payments where created at.month == month?
+    # Payment.where(:created_at => (Time.now.beginning_of_month)..Time.now).order("created_at ASC")
+    @payments = Payment.all
+    @month_payments = []
+    @payments.each do |p|
+      if p.created_at.month == month
+        @month_payments.push(p)
+      end
+    end
+    return @month_payments
+  end
+
+  def self.all_year_to_date
+    @payments = Payment.where(:created_at => (Time.now.beginning_of_year)..Time.now).order("created_at ASC")
+  end
+
+  def self.all_year_total
+    @payments = all_year_to_date
+    @total = 0.0
+    @payments.each do |p|
+       @total += p.amount
+    end
+    return @total
+  end
 end
--- app/models/service.rb
+++ app/models/service.rb
@@ -12,19 +12,6 @@ class Service < ActiveRecord::Base
     return @total
   end
 
-  def month_payment(month)
-    # payments where created at.month == month?
-    # Payment.where(:created_at => (Time.now.beginning_of_month)..Time.now).order("created_at ASC")
-    @payments = Payment.all
-    @month_total = 0.0
-    @payments.each do |p|
-      if p.created_at.month == month
-        @month_total+= p.amount
-      end
-    end
-    return @month_total
-  end
-
   def year_to_date
     @payments = Payment.where(:created_at => (Time.now.beginning_of_year)..Time.now, :service_id => id).order("created_at ASC")
   end
@@ -38,16 +25,4 @@ class Service < ActiveRecord::Base
      return @total
   end
 
-  def self.all_year_to_date
-    @payments = Payment.where(:created_at => (Time.now.beginning_of_year)..Time.now).order("created_at ASC")
-  end
-
-  def self.all_year_total
-    @payments = all_year_to_date
-    @total = 0.0
-    @payments.each do |p|
-       @total += p.amount
-    end
-    return @total
-  end
 end
